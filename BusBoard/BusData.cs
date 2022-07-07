48092117e42607deb92287ee3a79e143c40816cf
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-07 11:36:00 +0100

    Print out first five buses at a stop

---
+++ BusBoard/BusData.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace BusBoard;
+
+public class StopData
+{
+    public string name;
+    public Dictionary<string, List<BusData>> departures;
+}
+
+public class BusData
+{
+    public string line;
+    public string direction;
+    public string aimed_departure_time;
+    public string expected_departure_time;
+}
--- BusBoard/Program.cs
+++ BusBoard/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace BusBoard
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BusBoard
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            Console.WriteLine("Enter bus stop code:");
+            //string userInput = Console.ReadLine();
+            HttpClient client = new HttpClient();
             Console.WriteLine("Welcome to BusBoard!");
+            //var result = await client.GetStringAsync("https://transportapi.com/v3/uk/bus/stop/"+userInput+"/live.json?&app_id=a5babc51&app_key=f057beb130e643c45171f1ae19b3e4fd&group=no&limit=5");
+            var result = await client.GetStringAsync("https://transportapi.com/v3/uk/bus/stop/0500CCITY436/live.json?&app_id=a5babc51&app_key=f057beb130e643c45171f1ae19b3e4fd&group=no&limit=5");
+            var stopResult = JsonConvert.DeserializeObject<StopData>(result);
+
+
+            foreach (var bus in stopResult.departures["all"])
+            {
+                Console.WriteLine($"Line: {bus.line}, Destination: {bus.direction}, Time: {bus.aimed_departure_time}, Expected: {bus.expected_departure_time}");
+            }
+
+            Console.WriteLine();
         }
     }
+    
+    
 }
