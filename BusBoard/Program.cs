b50afd1320ca9076e30b33ac17316656aae5e575
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-07 11:57:44 +0100

    Neatened up the code and compartmentalised it into methods

--- BusBoard/Program.cs
+++ BusBoard/Program.cs
@@ -5,25 +5,39 @@ namespace BusBoard
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
+        {
+            PrintBusData(RetrieveBusData().Result);
+        }
+
+        private static string BusStopEntry()
         {
             Console.WriteLine("Enter bus stop code:");
-            //string userInput = Console.ReadLine();
+            return Console.ReadLine();
+        }
+
+        private static async Task<StopData> RetrieveBusData()
+        {
             HttpClient client = new HttpClient();
             Console.WriteLine("Welcome to BusBoard!");
-            //var result = await client.GetStringAsync("https://transportapi.com/v3/uk/bus/stop/"+userInput+"/live.json?&app_id=a5babc51&app_key=f057beb130e643c45171f1ae19b3e4fd&group=no&limit=5");
-            var result = await client.GetStringAsync("https://transportapi.com/v3/uk/bus/stop/0500CCITY436/live.json?&app_id=a5babc51&app_key=f057beb130e643c45171f1ae19b3e4fd&group=no&limit=5");
-            var stopResult = JsonConvert.DeserializeObject<StopData>(result);
+            var result = await client.GetStringAsync("https://transportapi.com/v3/uk/bus/stop/"+BusStopEntry()+"/live.json?&app_id=a5babc51&app_key=f057beb130e643c45171f1ae19b3e4fd&group=no&limit=5");
+            return JsonConvert.DeserializeObject<StopData>(result);
+        }
 
-
-            foreach (var bus in stopResult.departures["all"])
+        private static void PrintBusData(StopData stopResult)
+        {
+            if (stopResult != null)
             {
-                Console.WriteLine($"Line: {bus.line}, Destination: {bus.direction}, Time: {bus.aimed_departure_time}, Expected: {bus.expected_departure_time}");
+                foreach (var bus in stopResult.departures["all"])
+                {
+                    Console.WriteLine(
+                        $"Line: {bus.line}, Destination: {bus.direction}, Time: {bus.aimed_departure_time}, Expected: {bus.expected_departure_time}");
+                }
             }
-
-            Console.WriteLine();
+            else
+            {
+                Console.WriteLine("Invalid input :(");
+            }
         }
     }
-    
-    
 }
