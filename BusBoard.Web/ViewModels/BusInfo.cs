065517bef95b0adba48b21436ced8092a04e34d3
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-08 09:37:09 +0100

    Allows user to enter postcode on webpage and fetch two bus stops

--- BusBoard.Web/ViewModels/BusInfo.cs
+++ BusBoard.Web/ViewModels/BusInfo.cs
@@ -1,13 +1,27 @@
-﻿namespace BusBoard.Web.ViewModels
+﻿using BusBoard.Api;
+
+namespace BusBoard.Web.ViewModels
 {
     public class BusInfo
     {
+        private Api.BusBoard busBoard = new Api.BusBoard();
+        
         public BusInfo(string postCode)
         {
             PostCode = postCode;
         }
 
         public string PostCode { get; set; }
+
+        public bool ValidatePostcode()
+        {
+            return busBoard.ValidatePostcode(PostCode);
+        }
+
+        public List<Api.BusStop> GetStops()
+        {
+            return busBoard.GetBusStopsFromPostcode(busBoard.PerformPostcodeLookup(PostCode), 2);
+        }
 
     }
 }
